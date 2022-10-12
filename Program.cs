0c37d569f6ae639b2f4656ca626c28cdd3325564
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-12 04:18:31 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: Oct. 12, 2022
+* Assignment CIS214 Performance Assessment - Loops & Calculations
+*
+* Main application (program) class.
+* The main application class must meet the following requirements
+* Print a line that states "Your Name - Week 2 PA Loops & Calculations"
+*
+* Demonstrate a counter-controlled loop
+*   Calculate the sum of the integers from 1 to 10
+*   Print the running total
+*   Print the final total
+*
+* Demonstrate a condition-based loop
+*   Ask the user to enter an integer value (-1 to quit)
+*   Keep a running sum of the values
+*   Keep a count of how many values the user has entered
+*   Print the running total after each entry
+*   Print the final sum of all values entered
+*   Print a count of the values entered
+*
+\******************************************************************************/
+public class LoopCalculation
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Loops & Calculations
+
+## Main application (program) class
+
+The main application class must meet the following requirements
+
+- Print a line that states "Your Name - Week 2 PA Loops & Calculations"
+
+- Demonstrate a counter-controlled loop
+  - Calculate the sum of the integers from 1 to 10
+  - Print the running total
+  - Print the final total
+
+- Demonstrate a condition-based loop
+  - Ask the user to enter an integer value (-1 to quit)
+  - Keep a running sum of the values
+  - Keep a count of how many values the user has entered
+  - Print the running total after each entry
+  - Print the final sum of all values entered
+  - Print a count of the values entered
